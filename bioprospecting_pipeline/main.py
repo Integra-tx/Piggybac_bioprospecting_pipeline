4589f27c1a56c4b8d64028c5c79ce51d5fbf5294
Alejo945 <93728611+Alejo945@users.noreply.github.com>
2025-06-30 16:59:14 +0200

    Update main.py

--- bioprospecting_pipeline/main.py
+++ bioprospecting_pipeline/main.py
@@ -119,7 +119,7 @@ def main():
                     dataframe_pre_clustering.append(temporal_dataframe)
 
                                     # Define the column names for the DataFrame
-                columns = ["Accession", "Taxonomy", "Transposase", "Transposon", "CRD_motif", "DDE", "N-term", "No-nterm", "ttaa", "N_palindromes","palindromes", "SG", "Domains", "Clustered", "Full_dna"]
+                columns = ["Accession", "Taxonomy", "Transposase", "Transposon", "CRD_motif", "rDNA", "DDE", "N-term", "No-nterm", "ttaa", "N_palindromes","palindromes", "SG", "Domains", "Clustered", "Full_dna"]
                 flattened_list = flatten_deep(dataframe_pre_clustering)
                 final_pre_clustering_dataframe = pd.DataFrame(flattened_list, columns=columns)
                 final_pre_clustering_dataframe.to_csv(f'{output}/Pre_filtering_complete_data.tsv', sep='\t', index=False)
