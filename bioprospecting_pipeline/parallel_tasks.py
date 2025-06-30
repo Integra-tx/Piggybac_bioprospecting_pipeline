50eb8dfa84032b219902a373bfae9bc30f27956c
Alejo945 <93728611+Alejo945@users.noreply.github.com>
2025-06-30 11:03:11 +0200

    Update parallel_tasks.py

--- bioprospecting_pipeline/parallel_tasks.py
+++ bioprospecting_pipeline/parallel_tasks.py
@@ -423,16 +423,16 @@ def extract_dna(out_in, name, genome_paths, extension, out, complete_taxonomy_di
                                             rna_temp.write(str((record.seq[rna_begin:rna_end]).reverse_complement()).upper() + "\n")
 		    
 
-        try:
-            os.path.isfile(dna_filename)
-            pre_cluster_dataframe = orf_finder(dna_filename, complete_taxonomy_dict, rna_temp, min_orf_length, blast_path,blast_db)
-            os.remove(dna_filename)
-            print(f"Finished with {genome_name}")
-            return pre_cluster_dataframe
-        except:
-            with open('Genomes_without_hits.txt','a') as empty_file:
-                empty_file.write('dna_filename\n')
-            return None
+        # try:
+        os.path.isfile(dna_filename)
+        pre_cluster_dataframe = orf_finder(dna_filename, complete_taxonomy_dict, rna_temp, min_orf_length, blast_path,blast_db)
+        os.remove(dna_filename)
+        print(f"Finished with {genome_name}")
+        return pre_cluster_dataframe
+        # except:
+        #     with open('Genomes_without_hits.txt','a') as empty_file:
+        #         empty_file.write('dna_filename\n')
+        #     return None
 
 @ray.remote
 def genome_reader(combined_data):
